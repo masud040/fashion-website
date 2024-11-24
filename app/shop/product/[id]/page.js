438b973f69294c35bb70ef726dcf293bb89c50ca
masud040 <masudinfo962@gmail.com>
2024-11-24 17:12:06 +0600

    feat: add a function for dynamically generate metadata

--- app/shop/product/[id]/page.js
+++ app/shop/product/[id]/page.js
@@ -1,6 +1,17 @@
 import ProductDetailsContainer from "@/app/components/ProductDetails";
 import Breadcrumb from "@/app/components/shared/Bredcrumb";
+import { getSingleProduct } from "@/db/queries";
 
+// generate metadata
+export async function generateMetadata({ params }) {
+  const product = await getSingleProduct(params?.id);
+  return {
+    title: product.name,
+    openGraph: {
+      images: [product.thumbnail],
+    },
+  };
+}
 export default async function ProductPage({ params }) {
   const { id } = await params;
   return (
