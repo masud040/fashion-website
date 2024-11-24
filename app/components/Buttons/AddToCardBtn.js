18e8fae259a278a368409bed87eb62765b278ef6
masud040 <masudinfo962@gmail.com>
2024-11-24 17:19:08 +0600

    fix: stopPropagation when click add to cart button.

--- app/components/Buttons/AddToCardBtn.js
+++ app/components/Buttons/AddToCardBtn.js
@@ -1,6 +1,14 @@
+"use client";
 export const AddToCardBtn = () => {
+  function handleAddToCart(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
   return (
-    <button className="py-2.5 px-6 border rounded-lg body-semibold border-primary-dark text-primary-dark hover:bg-primary-darker transition-color duration-500 hover:text-background-light">
+    <button
+      onClick={handleAddToCart}
+      className="py-2.5 px-6 border rounded-lg body-semibold border-primary-dark text-primary-dark hover:bg-primary-darker transition-color duration-500 hover:text-background-light"
+    >
       Add To Cart
     </button>
   );
